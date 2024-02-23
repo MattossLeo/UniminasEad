10fc2947221ed2ce0c8f3811d213d77d2ed3dff7
Leonardo Mattos <leonardosousamattos@gmail.com>
2024-02-23 14:57:20 -0300

    Backup

---
+++ blocks/area/area-search.php
@@ -0,0 +1,49 @@
+<section class="area-search">
+    <div class="container">
+        <div class="row">
+            <div class="main__title--search">
+                <h2 class="main__title--h2 color-dark--green">Encontre seu curso</h2>
+                <div class="main__subititle--search">
+                    <img width="60" height="56" src="<?php echo get_template_directory_uri()?>/assets/img/search-certifier.svg" alt="search-certifier">
+                    <p class="main__text--subtitle color-dark--green">Cursos reconhecidos pelo MEC, por Portaria nº 314, de 2 de março de 2020.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3">
+                <div class="search--course">
+                    <div class="input__search">
+                        <input class="main__input--search" id="searchCourse" type="search" placeholder="Digite o curso que você procura">
+                        <div class="main__icon--search">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="main__card--courses">
+                    <div class="row align-items-end">
+                        <div class="col-lg-9">
+                            <div class="card__courses--title">
+                                <h2 class="tittle-courses color-white">MEDIAÇÃO E GESTÃO DE CONFLITOS</h2>
+                            </div>
+                            <div class="card__courses--content">
+                                <p class="courses-texts color-white">Contribua na resolução de conflitos familiares, empresariais e comunitários, promovendo a comunicação eficaz entre as partes.</p>
+                            </div>
+                            <div class="card__courses--price">
+                                <p class="main-prices color-white"><b>12x25,90</b>&nbsp;&nbsp;&nbsp;<s class="fake-price">12x 39,90</s></p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="main__card--btn">
+                                <a class="btn-courses" href="#">CONHEÇER O CURSO</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
