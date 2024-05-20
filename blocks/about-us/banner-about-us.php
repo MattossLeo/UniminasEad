342a6564e4695a083b19d3a49d63474332fcddee
MattossLeo <leonardosousamattos@gmail.com>
2024-05-20 11:06:04 -0300

    add new page and fix footer

---
+++ blocks/about-us/banner-about-us.php
@@ -0,0 +1,9 @@
+<section class="banner__about-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2 class="main__title--h2 title__banner--about-us">Sobre Nós</h2>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ blocks/about-us/description-about-us.php
@@ -0,0 +1,35 @@
+<section class="description-about-us">
+    <div class="container">
+        <div class="row">
+            <div class="main__description--about-us">
+                <h2 class="main__title--h2 title-who-are-we">Quem Somos Nós</h2>
+                <p class="main__text--who-are-we">A história do Uniminas EAD inicia com a realização do sonho de um grupo de empresários, que buscavam atender a crescente demanda de alunos para cursos de pós-graduação.
+                    <br><br>Dessa forma, foi criado o Uniminas EAD, uma entidade voltada para a oferta de serviços educacionais em nível superior.
+                    <br><br>O objetivo do Uniminas EAD é formar diplomados nas diferentes áreas do conhecimento, capacitando-os para a inserção em setores profissionais e para a participação no desenvolvimento da sociedade brasileira, além de colaborar na sua formação contínua. A instituição também promove a divulgação de conhecimentos culturais, científicos e técnicos que constituem o patrimônio da humanidade, comunicando o saber através do ensino, publicações e outras formas de comunicação.
+                </p>
+            </div>
+            <div class="main--mission-vision-values">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="main__card--mission-vision-values">
+                            <h3 class="title-card__mission-vision-values">Missão</h3>
+                            <p class="text-card__mission-vision-values">A Uniminas EAD se compromete a oferecer qualidade em conhecimento e cultura de forma confiável e eficiente, proporcionando aos alunos a oportunidade de construir uma base profissional ética.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="main__card--mission-vision-values">
+                            <h3 class="title-card__mission-vision-values">Visão</h3>
+                            <p class="text-card__mission-vision-values">A Uniminas EAD tem a visão de se tornar uma instituição modelo no país na oferta de cursos, primando sempre pela inovação tecnológica, excelência no atendimento e valorização do serviço oferecido.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="main__card--mission-vision-values">
+                            <h3 class="title-card__mission-vision-values">Valores</h3>
+                            <p class="text-card__mission-vision-values"> Atuar com ética e integridade, com base na honestidade, na moralidade, na coerência, assegurando o padrão de qualidade e excelência em nossos serviços com simplicidade que se ajustam às necessidades dos clientes/alunos.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
